eeb7587a7f70e27cdf29c91cae3db0b683afbe6a
Wiraya <wiraya.h@rmutsvmail.com>
2023-09-01 16:27:45 +0700

    Vehicle_App.py Vehicle.py

--- Lab6_OOP_Concept/Vehicle.py
+++ Lab6_OOP_Concept/Vehicle.py
@@ -1,44 +1,45 @@
-"""
-OOP Exercise Chapter 6
 
-1. จงเขียนโปรแกรมภาษาไพธอนเพื่อสร้างคลาสพาหนะชื่อ Vehicle ที่ประกอบไปด้วยคุณลักษณะ (attributes) คือ
-ยี่ห้อรถ (brand)
-รุ่นรถ (model)
-สีรถ (color)
-ความรเร็วสูงสุด (max speed)
-ราคา (price)
+# class attribute
+class vehicle:
+    my_vehicle = []
 
-2.จากข้อที่ 1 เขียนโปรแกรมเพื่อสร้างวัตถุ (object) จากคลาส Vehicle โดยรับข้อมูลจากผู้ใช้ตามคุณลักษณะ (attributes)ของคลาส
-จากนั้นแสดงข้อมูลทางหน้าจอภาพ
-
-15 นาที
-"""
-class Vehicle:
     def __init__(self,brand,model,color,maxspeed,price):
         self.brand = brand
         self.model = model
         self.color = color
         self.maxspeed = maxspeed
         self.price = price
-        #self.my_vihecle.append(self)
 
-    def Vehicle_info(self):
-        print(f'brand:{self.brand}'
-              f' model:{self.model}'
-              f' color:{self.color}'
-              f' maxspeed:{self.maxspeed}'
-              f' price:{self.price}')
-# vc = []
 
-# v = int(input('How many Vehicle? : '))
-# for i in range(v):
-#     s = Vehicle()
-#     print(f"Please, enter Vehicle info {i+1}:")
-#     s.brand = input("Enter brand: ")
-#     s.model = input("Enter model: ")
-#     s.color = input("Enter color: ")
-#     s.maxspeed = input("Enter maxspeed: ")
-#     s.price = input("Enter price: ")
-#     vc.append(s)
-# for i in vc:
-#     i.Vehicle_info()
+# display object atteibute
+    def vehicle_detail(self):
+        print(f'brand:{self.brand} '
+              f'model:{self.model} '
+              f'color:{self.color} '
+              f'maxspeed:{self.maxspeed} '
+              f'price:{self.price}')
+
+    def delete_vehicle(self,index):
+        self.my_vehicle.pop(index)
+
+    def edit_vehicle_price(self,index,new_price):     # edit vehicle
+        self.my_vehicle[index].price = new_price
+
+    def edit_vehicle_color(self,index,new_color):
+        self.my_vehicle[index].color = new_color
+
+
+#std = []
+#n = int(input('How many Vehicle : '))
+#for i in range(n):
+#    s = Vehicle()
+#    print(f"please, Enter brand info:")
+#    s.brand = input("Enter brand:")
+#    s.model = input("Enter model:")
+#    s.color = input("Enter color:")
+#    s.maxspeed = input("Enter maxspeed:")
+#    s.price = input("Enter price:")
+#    std.append(s)
+
+#for i in std:
+#    i.Vehicle_info()
--- Lab6_OOP_Concept/Vehicle_App.py
+++ Lab6_OOP_Concept/Vehicle_App.py
@@ -1,47 +1,97 @@
-from Vehicle import Vehicle
+from Vehicle import vehicle
+
 
 # option
 def display_option():
-    print("Welcom to vehicle Data Store  System (VDSS)")
+    print("Welcome to Vehicle Data Store System (VDSS)")
+    print("\n-------------------------------")
     print("1.Add Vehicle")
-    print("2.Display all Vehicle")
-    print("3.exit")
-    select = int(input("select(1-3)? : "))
+    print("2.Display all vehicle")
+    print("3.Delete Vehicle")
+    print("4.Edit Vehicle Price")
+    print("5.Edit Vehicle Color")
+    print("6.exit")
+    print("-------------------------------\n")
+    select = int(input("select (1-5)?: "))
     if select == 1:
         input_vehicle_data()
     elif select == 2:
-        display_option()
+        display_vehicle()
     elif select == 3:
+        delete_vehicle()
+    elif select == 4:
+        edit_vehicle_price()
+    elif select == 5:
+        edit_vehicle_color()
+    elif select == 6:
         print("Good Bye.")
         exit(0)
     else:
-        print("Please, select number 1-3.")
-#add data
+        print("Pleaes, select number 1-5")
+
+
+# Add Data
 def input_vehicle_data():
     brand = input("Enter vehicle brand: ")
-    modal = input("Enter vehicle model: ")
+    model = input("Enter vehicle model: ")
     color = input("Enter vehicle color: ")
-    maxspeed = int(input("Enter vehicle max sprrd:"))
-    price = float(input("Enter vehicle  price: "))
+    maxspeed = input("Enter vehicle maxspeed: ")
+    price = float(input("Enter vehicle price: "))
 
-    my_vehicle.append(Vehicle(brand,modal,color,maxspeed,price))
-    print("\n------------------------------------")
-    print("Already add vehicle to store. ")
-    print("------------------------------------\n")
 
-#displaydata
-def display_Vehicle():
-    if len(my_vehicle) ==0:
-        print("You had no vehicle data.")
+    vehicle.my_vehicle.append(vehicle(brand,model,color,maxspeed,price))
+    print("\n-------------------------------")
+    print("Already add vehicle to store.")
+    print("-------------------------------\n")
+
+
+# Display data
+def display_vehicle():
+    if len(vehicle.my_vehicle) ==0:
+        print("Yot had no vehicle data.")
     else:
-        print(f'You had {len(my_vehicle)} following:')
-        for x in my_vehicle:
+        print(f'You had {len(vehicle.my_vehicle)} following.')
+        n = 1 # count
+        for x in vehicle.my_vehicle:
+            print(f'[{n}]:',end=" ")
             x.vehicle_detail()
+            n = n+1
         print("\n")
 
 
-#run
-my_vehicle = []
+# delete vehicle
+def delete_vehicle():
+    display_vehicle() # display all data in list
+    if len(vehicle.my_vehicle) != 0:
+        s = int(input("Select to delete?: "))
+
+        vehicle.delete_vehicle(vehicle, s-1)
+        print("\n-------------------------------")
+        print("Your data has been deleted.")
+        print("-------------------------------\n")
+
+
+# edit vehicle price
+def edit_vehicle_price():
+    display_vehicle()
+    if len(vehicle.my_vehicle) != 0:
+        s = int(input("Select to edit?: "))
+
+        print(f'Previous price: {vehicle.my_vehicle[s-1].price}')
+        new_price = float(input("New price: "))
+        vehicle.edit_vehicle_price(vehicle,s-1,new_price)
+
+def edit_vehicle_color():
+    display_vehicle()
+    if len(vehicle.my_vehicle) != 0:
+        s = int(input("Select to edit?: "))
+
+        print(f'Previous color: {vehicle.my_vehicle[s-1].color}')
+        new_color = input("New Color: ")
+        vehicle.edit_vehicle_color(vehicle,s-1,new_color)
+
+
+# my_vehicle = []
 s = 0
 while s == 0:
     display_option()
